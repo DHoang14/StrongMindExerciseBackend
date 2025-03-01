7095c7db9a2e6affcc7107219e3f6b851b983176
DHoang14 <dhoang9@asu.edu>
2025-03-01 00:28:13 -0700

    Changed to serverless

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-//const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 const app = express();
 const cors = require('cors');
 const corsOptions = require('../config/corsOptions');
@@ -13,13 +13,13 @@ app.use(creds);
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/toppings', require('../routes/toppings'));
-app.use('/pizza', require('../routes/pizza'));
+app.use('/.netlify/functions/server/toppings', require('../routes/toppings'));
+app.use('/.netlify/functions/server/pizza', require('../routes/pizza'));
 
 app.all('*', (req, res) => {
     res.status(404);
     res.type('txt').send("404 NOT FOUND");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//module.exports.handler = serverless(app)
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports.handler = serverless(app)
